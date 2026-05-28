f29631a737a7caa46d6c93da0165e3776fece952
NextGen <120759096+NextGen-cgp@users.noreply.github.com>
2026-05-28 12:32:42 +0200

    Refactor VPN_INVGATE.bat for improved logging and structure

--- VPN_INVGATE.bat
+++ VPN_INVGATE.bat
@@ -1,60 +1,62 @@
 @echo off
 setlocal enableextensions
 
-REM Usar reg.exe de 64 bits si el agente ejecuta en contexto 32 bits
+set "PS_EXE=%windir%\System32\WindowsPowerShell\v1.0\powershell.exe"
+if exist "%windir%\Sysnative\WindowsPowerShell\v1.0\powershell.exe" set "PS_EXE=%windir%\Sysnative\WindowsPowerShell\v1.0\powershell.exe"
+
 set "REG_EXE=%windir%\System32\reg.exe"
 if exist "%windir%\Sysnative\reg.exe" set "REG_EXE=%windir%\Sysnative\reg.exe"
 
-REM Clave de deteccion
+set "BASE_URL=https://raw.githubusercontent.com/NextGen-cgp/test-deploy/main"
+set "WORKDIR=%ProgramData%\Clarel\InvGate\02VPN"
+set "CONF_FILE=%WORKDIR%\FortiClient_02_VPN_USERS_CLAREL.xml"
+set "LOG_FILE=%WORKDIR%\install.log"
+
 set "DETECTION_KEY=HKLM\SOFTWARE\Dia-SCCM\02_VPN_USERS_CLAREL_VDF"
 set "DETECTION_VALUE=Installed"
 
-REM 0. Comprobar si ya esta aplicado
-"%REG_EXE%" query "%DETECTION_KEY%" /v "%DETECTION_VALUE%" >nul 2>&1
+set "FCCONFIG=%ProgramFiles%\Fortinet\FortiClient\FCConfig.exe"
+if not exist "%FCCONFIG%" set "FCCONFIG=%ProgramFiles(x86)%\Fortinet\FortiClient\FCConfig.exe"
 
+if not exist "%WORKDIR%" mkdir "%WORKDIR%"
+
+echo [%date% %time%] Inicio despliegue VPN > "%LOG_FILE%"
+
+"%REG_EXE%" query "%DETECTION_KEY%" /v "%DETECTION_VALUE%" >> "%LOG_FILE%" 2>&1
 if %errorlevel%==0 (
-    echo La configuracion VPN ya estaba aplicada. No se ejecuta de nuevo.
+    echo [%date% %time%] Ya estaba aplicada. >> "%LOG_FILE%"
     exit /b 0
 )
 
-REM 1. URL base de GitHub RAW
-set "BASE_URL=https://raw.githubusercontent.com/NextGen-cgp/test-deploy/main"
-
-REM 2. Carpeta local de trabajo
-set "WORKDIR=%ProgramData%\Clarel\InvGate\02VPN"
-
-REM 3. Ruta local donde se guardara el .reg descargado
-set "REG_FILE=%WORKDIR%\VPN_USERS_CLAREL_FORTI.reg"
-
-REM 4. Crear carpeta local si no existe
-if not exist "%WORKDIR%" mkdir "%WORKDIR%"
-
-REM 5. Descargar el .reg desde GitHub a la carpeta local
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^
-  "$ErrorActionPreference='Stop';" ^
-  "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;" ^
-  "Invoke-WebRequest -UseBasicParsing -Uri '%BASE_URL%/VPN_USERS_CLAREL_FORTI.reg' -OutFile '%REG_FILE%'"
-
-if errorlevel 1 (
-    echo Error descargando VPN_USERS_CLAREL_FORTI.reg desde GitHub
-    exit /b 10
-)
-
-REM 6. Importar el .reg ya descargado
-"%REG_EXE%" import "%REG_FILE%"
-
-if errorlevel 1 (
-    echo Error importando VPN_USERS_CLAREL_FORTI.reg
+if not exist "%FCCONFIG%" (
+    echo [%date% %time%] No se encontro FCConfig.exe >> "%LOG_FILE%"
     exit /b 20
 )
 
-REM 7. Crear clave de deteccion
-"%REG_EXE%" add "%DETECTION_KEY%" /v "%DETECTION_VALUE%" /t REG_SZ /d "1" /f
+"%PS_EXE%" -NoProfile -ExecutionPolicy Bypass -Command ^
+  "$ErrorActionPreference='Stop';" ^
+  "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;" ^
+  "Invoke-WebRequest -UseBasicParsing -Uri '%BASE_URL%/FortiClient_02_VPN_USERS_CLAREL.xml' -OutFile '%CONF_FILE%'" >> "%LOG_FILE%" 2>&1
 
 if errorlevel 1 (
-    echo Error creando clave de deteccion
+    echo [%date% %time%] Error descargando XML desde GitHub >> "%LOG_FILE%"
+    exit /b 10
+)
+
+"%FCCONFIG%" -m all -f "%CONF_FILE%" -o import -i 1 -p "12345678" >> "%LOG_FILE%" 2>&1
+
+if errorlevel 1 (
+    echo [%date% %time%] Error importando VPN con FCConfig >> "%LOG_FILE%"
     exit /b 30
 )
 
-echo Configuracion VPN importada correctamente
+"%REG_EXE%" add "%DETECTION_KEY%" /v "%DETECTION_VALUE%" /t REG_SZ /d "1" /f >> "%LOG_FILE%" 2>&1
+
+if errorlevel 1 (
+    echo [%date% %time%] Error creando clave de deteccion >> "%LOG_FILE%"
+    exit /b 40
+)
+
+echo [%date% %time%] VPN importada correctamente >> "%LOG_FILE%"
+exit /b 0
 exit /b 0
